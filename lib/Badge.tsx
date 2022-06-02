b3de10322e382edcb04c35d6e6b1c98e11361ac1
Paweł Kuźnik <pawel.kuznik@gmail.com>
2022-06-03 00:18:11 +0200

    Improved styles of the badge

--- lib/Badge.tsx
+++ lib/Badge.tsx
@@ -4,7 +4,6 @@ import css from "./Badge.module.css";
  *  A small component to show a badge with text.
  */
 export default function Badge(props: BadgeProps) {
-    console.log('class', css);
     return (
         <span className={css.badge}>{props.label}</span>
     );
