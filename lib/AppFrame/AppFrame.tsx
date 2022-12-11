79ac8c25d2fd21ab3618e14d2cb7a80afe567be8
Paweł Kuźnik <pawel.kuznik@gmail.com>
2022-12-11 14:45:15 +0100

    Added styles for the AppFrame that put top on the top and bottom on the botttom.

--- lib/AppFrame/AppFrame.tsx
+++ lib/AppFrame/AppFrame.tsx
@@ -1,12 +1,30 @@
-import { ReactNode } from "react";
+import React, { Component, ReactNode } from "react";
 import "./AppFrame.css";
 
 export interface AppFrameProps {
+
+    /**
+     *  A ReactNode to insert into the top part of the AppFrame.
+     */
+    top?: ReactNode;
+
+    /**
+     *  A ReactNode to insert into the bottom part of the AppFrame.
+     */
+    bottom?: ReactNode;
+    
+    /**
+     *  The content of the AppFrame.
+     */
     children?: ReactNode;
 };
 
-export function AppFrame({ children }: AppFrameProps) {
-    return (<div className="cogi-uikit-appframe">
-        {children}
-    </div>);
+export function AppFrame({ top, bottom, children }: AppFrameProps) {
+    return (
+        <div className="cogi-uikit-appframe">
+            {top && (<div className="cogi-uikit-appframe-top">{top}</div>)}
+            {children}
+            {bottom && (<div className="cogi-uikit-appframe-bottom">{bottom}</div>)}
+        </div>
+    );
 };
--- lib/SelectableUI.ts
+++ lib/SelectableUI.ts
@@ -1,6 +1,25 @@
+/**
+ *  This is an interface descriging a UI part that is selectable.
+ */
 export interface SelectableUIProps {
+    
+    /**
+     *  Should the component be selected on the momenent of instantiation?
+     */
     selected?: boolean;
+    
+    /**
+     *  Is the component currently selectable?
+     */
     selectable?: boolean;
+
+    /**
+     *  A callback called when user wants to select the component.
+     */
     onSelected?: () => void;
+    
+    /**
+     *  A callback called when user wants to deselect the component.
+     */
     onDeselected?: () => void;
 };
