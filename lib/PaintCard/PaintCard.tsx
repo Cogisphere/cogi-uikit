8006bf38e3b2ee4eb40084ba24a71129b9b8c6ff
pawel-kuznik <pawel.kuznik@gmail.comm>
2023-01-15 23:07:41 +0100

    Added onClick handler to PaintCard

--- lib/PaintCard/PaintCard.tsx
+++ lib/PaintCard/PaintCard.tsx
@@ -3,16 +3,25 @@ import { Paint } from "@cogisphere/cogi-colors";
 import "./PaintCard.css";
 
 export interface PaintCardProps {
+
+    /**
+     *  The paint to show.
+     */
     paint: Paint;
+
+    /**
+     *  A callback triggered when user clicks on a paint card.
+     */
+    onClick?: (paint: Paint) => void;
 };
 
 /**
  *  This is a component that renders a small card with the paint
  *  color and name.
  */
-export function PaintCard({ paint }: PaintCardProps) {
+export function PaintCard({ paint, onClick }: PaintCardProps) {
 
-    return (<div className="cogi-uikit-paintcard">
+    return (<div className="cogi-uikit-paintcard" onClick={() => onClick?.(paint) }>
         <span className="cogi-uikit-paintcard-label">{paint.name}</span>
         <div className="cogi-uikit-paintcard-color" style={{ backgroundColor: paint.color.toHex() }}></div>
     </div>);
